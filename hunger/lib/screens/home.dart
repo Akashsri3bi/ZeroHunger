01bfb761b92aed4ba33672174d7b5573b3613e3e
Akash srivastava <akashuna3@gmail.com>
2022-02-01 22:11:23 +0530

    project

---
+++ hunger/lib/screens/home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('APP_NAME'),
+        backgroundColor: Colors.green[500],
+      ),
+      body: Container(),
+    );
+  }
+}
