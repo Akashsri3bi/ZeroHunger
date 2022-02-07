53bb6dc2dacbb37db90c903561525ed58a806c11
Akash srivastava <akashuna3@gmail.com>
2022-02-07 16:20:40 +0530

    resolvederrors

--- hunger/lib/screens/login.dart
+++ hunger/lib/screens/login.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:hunger/config/theme.dart';
-import 'package:hunger/screens/home.dart';
-import 'package:hunger/screens/splash_screen.dart';
 
 class LoginPage extends StatelessWidget {
   @override
@@ -11,7 +8,6 @@ class LoginPage extends StatelessWidget {
 }
 
 class Login extends StatefulWidget {
-
   @override
   _LoginState createState() => _LoginState();
 }
@@ -20,177 +16,168 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Container(
-            padding: const EdgeInsetsDirectional.fromSTEB(0,100,0,0),
-            child: const Text(
-                "Welcome",
-                style: TextStyle(
-                  fontSize: 55,
-                  fontWeight: FontWeight.bold,
-                )
-            )
-          ),
-        Container( // TEXT
-          padding: const EdgeInsetsDirectional.fromSTEB(30,40,30,15),
-          child: Stack(
-            children: <Widget>[
-              Container(
-                  child: const Text(
-                      "Login, please",
-                      style: TextStyle(
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold,
-                      )
-                  )
-              ),
-            ],
-          ),
-        ),
-        Container( // for email
-          padding: const EdgeInsetsDirectional.fromSTEB(45,40,45,27),
-          child: Column(
-            children: const <Widget>[
-              TextField(
-                style: TextStyle(
-                    fontSize: 22,
-                    fontFamily: 'helvetica'
-                ),
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                    labelText: 'Email',
-                    labelStyle: TextStyle(
-                        fontFamily: 'Montserrat',
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
-                        color: Colors.grey
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.green,
-                        ),
-                    ),
-                ),
-              ),
-            ],
-          ),
-        ),
-        Container( // for password
-          padding: const EdgeInsetsDirectional.fromSTEB(45,0,45,28),
-          child: Column(
-            children: const <Widget>[
-              TextField(
-                style: TextStyle(
-                    fontSize: 22,
-                    fontFamily: 'helvetica'
-                ),
-                decoration: InputDecoration(
-                    labelText: 'Password',
-                    labelStyle: TextStyle(
-                        fontFamily: 'Montserrat',
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
-                        color: Colors.grey
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.green,
-                        ),
-                    ),
-                ),
-                obscureText: true,
-              ),
-            ],
-          ),
-        ),
-
-          //const SizedBox(height: 5.0),
-        Container(
-          alignment: const Alignment(1.0, 0.0),
-          padding: const EdgeInsetsDirectional.fromSTEB(0,0,25,13),
-          child: const InkWell(
-            child: Text(
-              'Forgot Password',
-            style: TextStyle(
-              fontSize: 15,
-              color: Colors.green,
-              fontWeight: FontWeight.bold,
-              fontFamily: "Helvetica",
-              decoration: TextDecoration.underline
-            ),),
-          ),
-        ),
-        const SizedBox(height: 30.0),
-          Container(
-            padding: const EdgeInsetsDirectional.fromSTEB(45, 0, 45, 0 ),
-            height: 42,
-            child: Material(
-              borderRadius: BorderRadius.circular(20),
-              shadowColor: Colors.greenAccent,
-              color: Colors.green[600],
-              elevation: 5,
-              child: GestureDetector(
-                onTap: () {},
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+                padding: const EdgeInsets.only(top: 50),
                 child: const Center(
-                  child: Text(
-                    'LOGIN',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Helvetica'
-                    ),
-                  ),
-                ),
+                  child: Text("Welcome",
+                      style: TextStyle(
+                        fontSize: 55,
+                        fontWeight: FontWeight.bold,
+                      )),
+                )),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: Stack(
+                children: [
+                  Container(
+                      child: const Text("Login, please",
+                          style: TextStyle(
+                            fontSize: 40,
+                            color: Colors.red,
+                            fontWeight: FontWeight.bold,
+                          ))),
+                ],
               ),
             ),
-          ),
-          const SizedBox(height: 25),
-          Container(
-            padding: const EdgeInsetsDirectional.fromSTEB(45, 0, 45, 0),
-            height: 42,
-            child: Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  width: 1.0,
-                ),
-                color: Colors.transparent,
-                borderRadius: BorderRadius.circular(20.0),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const <Widget> [
-                  SizedBox(width: 10),
-                  Center(
-                    child: Text(
-                      'Login with Google',
-                      style: TextStyle(
-                        fontFamily: 'helvetica',
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold
+            Container(
+              // for email
+              padding: const EdgeInsetsDirectional.fromSTEB(45, 40, 45, 27),
+              child: Column(
+                children: const <Widget>[
+                  TextField(
+                    style: TextStyle(fontSize: 22, fontFamily: 'helvetica'),
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      labelStyle: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: Colors.grey),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.green,
+                        ),
                       ),
                     ),
                   ),
                 ],
               ),
             ),
-          ),
-          const SizedBox(height: 45),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const <Widget>[
-              Text(
-                  'New here ?',
-                style: TextStyle(
-                  fontFamily: 'Helvetica',
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
+            Container(
+              // for password
+              padding: const EdgeInsetsDirectional.fromSTEB(45, 0, 45, 28),
+              child: Column(
+                children: const <Widget>[
+                  TextField(
+                    style: TextStyle(fontSize: 22, fontFamily: 'helvetica'),
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      labelStyle: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: Colors.grey),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.green,
+                        ),
+                      ),
+                    ),
+                    obscureText: true,
+                  ),
+                ],
+              ),
+            ),
+
+            //const SizedBox(height: 5.0),
+            Container(
+              alignment: const Alignment(1.0, 0.0),
+              padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 25, 13),
+              child: const InkWell(
+                child: Text(
+                  'Forgot Password',
+                  style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.green,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "Helvetica",
+                      decoration: TextDecoration.underline),
                 ),
               ),
-              SizedBox(width: 8),
-              InkWell(
-                child: Text('Signup',
+            ),
+            const SizedBox(height: 30.0),
+            Container(
+              padding: const EdgeInsetsDirectional.fromSTEB(45, 0, 45, 0),
+              height: 42,
+              child: Material(
+                borderRadius: BorderRadius.circular(20),
+                shadowColor: Colors.greenAccent,
+                color: Colors.green[600],
+                elevation: 5,
+                child: GestureDetector(
+                  onTap: () {},
+                  child: const Center(
+                    child: Text(
+                      'LOGIN',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Helvetica'),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 25),
+            Container(
+              padding: const EdgeInsetsDirectional.fromSTEB(45, 0, 45, 0),
+              height: 42,
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 1.0,
+                  ),
+                  color: Colors.transparent,
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const <Widget>[
+                    SizedBox(width: 10),
+                    Center(
+                      child: Text(
+                        'Login with Google',
+                        style: TextStyle(
+                            fontFamily: 'helvetica',
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 45),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const <Widget>[
+                Text(
+                  'New here ?',
+                  style: TextStyle(
+                    fontFamily: 'Helvetica',
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(width: 8),
+                InkWell(
+                    child: Text(
+                  'Signup',
                   style: TextStyle(
                     color: Colors.green,
                     fontFamily: 'helvetica',
@@ -198,13 +185,12 @@ class _LoginState extends State<Login> {
                     decoration: TextDecoration.underline,
                     fontSize: 17,
                   ),
-                )
-              ),
-            ],
-          )
-        ],
+                )),
+              ],
+            )
+          ],
+        ),
       ),
-
     );
   }
 }
--- hunger/lib/screens/splash_screen.dart
+++ hunger/lib/screens/splash_screen.dart
@@ -25,36 +25,27 @@ class _SplashState extends State<Splash> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-          children: [
-            Container(
-              decoration: const BoxDecoration(
-                color: Color(0xffffffcc),
+      body: Column(children: [
+        const Padding(
+          padding: EdgeInsets.only(top: 220, left: 50, right: 80),
+          child: Text(
+            'Hungry',
+            style: TextStyle(fontSize: 45, fontWeight: FontWeight.bold),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 40),
+            child: Container(
+              decoration: BoxDecoration(
+                image:
+                    const DecorationImage(image: AssetImage('assets/logo.png')),
+                borderRadius: BorderRadius.circular(80),
               ),
             ),
-            const Padding(
-              padding: EdgeInsetsDirectional.fromSTEB(0, 125, 0, 50),
-              child: Text(
-                'End Hunger',
-                style: TextStyle(fontSize: 45, fontWeight: FontWeight.bold),
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 40),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: const Color(0xffffffcc),
-                    image: const DecorationImage(
-                        image: AssetImage('assets/logo.png')
-                    ),
-                    borderRadius: BorderRadius.circular(80),
-                  ),
-                ),
-              ),
-            ),
-          ]
-      ),
+          ),
+        ),
+      ]),
     );
   }
 }
