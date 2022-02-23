ecefe56aaec4c05322a5bb6a08a697ce857b8eee
selindoga <73192627+selindoga@users.noreply.github.com>
2022-02-23 09:35:55 +0300

    change api credentials

--- hunger/lib/repository/employment_repo.dart
+++ hunger/lib/repository/employment_repo.dart
@@ -4,10 +4,11 @@ import 'package:hunger/models/employment_details.dart';
 import 'package:http/http.dart' as http;
 
 //Selin  , Enter your credentials here where I have put mine. that is app id and app key down in BaseUrl ;
+// I changed the credentials with mine, they are new now
 class EmploymentRepository {
   static Future<List<Result>> getJobs(String jobTitle) async {
     String Baseurl =
-        'http://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=f4d924f2&app_key=f5ae6c1deeab44dce6d5e14ed2dcad6e&results_per_page=20&what=$jobTitle&content-type=application/json';
+        'http://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=8f55b34f&app_key=db42e5a4d6af37c8d1edd36768ccb306&results_per_page=20&what=$jobTitle&content-type=application/json';
     final response = await http.get(Uri.parse(Baseurl));
     try {
       final resposeJson = jsonDecode(response.body)['results'] as List;
