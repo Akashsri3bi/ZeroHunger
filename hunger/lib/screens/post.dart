cdb692a8e4b2a8aa7d8eeccfdb91af4639f176a5
selindoga <73192627+selindoga@users.noreply.github.com>
2022-02-26 15:24:45 +0300

    changes

--- hunger/lib/screens/post.dart
+++ hunger/lib/screens/post.dart
@@ -4,11 +4,17 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:async';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:path_provider/path_provider.dart';
 
 final FirebaseAuth auth = FirebaseAuth.instance;
+File? file;
 final User? currentUser = auth.currentUser;
-late File? file;
 String photoUrl = currentUser!.photoURL!;
+bool fileChosen = false;
+final userRef = FirebaseFirestore.instance.collection("Person");
+final postRef = FirebaseFirestore.instance.collection("Post");
 
 class Post extends StatefulWidget {
   @override
@@ -16,10 +22,15 @@ class Post extends StatefulWidget {
 }
 
 class _PostState extends State<Post> {
-  bool changed = false;
+  File otherFile = File("file.txt");
+  bool isUploading = false;
   int selectedIndex = 0;
+  TextEditingController textTitle = TextEditingController();
+  TextEditingController textDetailed = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    makeAnotherFile();
     return Material(
       child: Scaffold(
         appBar: AppBar(
@@ -32,17 +43,13 @@ class _PostState extends State<Post> {
           ),
           backgroundColor: Theme.of(context).backgroundColor,
           actions: [ InkWell(
-              onTap: () {
-                if (changed) {
-                  //Code to be executed if there is some material post .
-                }
-              },
+              onTap: () => isUploading ? null : () => handleSubmit(),
               splashColor: Colors.white,
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Icon(
                   Icons.send,
-                  color: changed ? Colors.green : Colors.grey,
+                  color: isUploading ? Colors.green : Colors.grey,
                 ),
               ),
             ),
@@ -50,26 +57,31 @@ class _PostState extends State<Post> {
         ),
         body: ListView(
           children: <Widget> [
-            Visibility( //this is the area to display image
-              child: Container(
-                height: 220,
-                width: MediaQuery.of(context).size.width * 0.8,
-                child: Center(
-                  child: AspectRatio(
-                    aspectRatio: 16/9,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        //image: DecorationImage(
-                        //  fit: BoxFit.cover,
-                        //  image: FileImage(file!),
-                        //)
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              visible: true,//doesImageExist(), //display it if there is an image
-            ),
+            isUploading ? LinearProgressIndicator() : Text(""),
+            //remove this part and connect posting with firebase (with only the texts data)
+
+            //Visibility(
+            //  //this is the area to display image
+            //  child: Container(
+            //    height: 220,
+            //    width: MediaQuery.of(context).size.width * 0.8,
+            //    child: Center(
+            //      child: AspectRatio(
+            //        aspectRatio: 16/9,
+            //        child: Container(
+            //          decoration: BoxDecoration(
+            //            image: DecorationImage(
+            //              fit: BoxFit.cover,
+            //              image: FileImage(displayImage()!),
+            //            )
+            //          ),
+            //        ),
+            //      ),
+            //    ),
+            //  ),
+            //  visible: doesImageExist(), //display it if there is an image
+            //  //todo: BUG it does not display when an image was chosen
+            //),
             Padding(
               padding: const EdgeInsetsDirectional.fromSTEB(10, 20, 10, 0),
               child: Expanded(
@@ -78,7 +90,8 @@ class _PostState extends State<Post> {
                     ListTile(
                       title: Container(
                         width: 250.0,
-                        child: const TextField(
+                        child: TextField(
+                          controller: textTitle,
                           decoration: InputDecoration(
                             hintText: "Write a title..",
                             border: InputBorder.none,
@@ -90,8 +103,9 @@ class _PostState extends State<Post> {
                         ),
                       ),
                     ), const Divider(),
-                    const ListTile(
+                    ListTile(
                       title: TextField(
+                        controller: textDetailed,
                         decoration: InputDecoration(
                           hintText: "Write something...",
                           border: InputBorder.none,
@@ -109,25 +123,85 @@ class _PostState extends State<Post> {
             ),
           ],
         ),
-        //bottomNavigationBar: BottomNavigationBar(
-        //  items: const <BottomNavigationBarItem>[
-        //    BottomNavigationBarItem(
-        //      icon: Icon(Icons.camera_alt),
-        //      label: 'Add photo',
-        //    ),
-        //  ],
-        //  currentIndex: selectedIndex,
-        //  onTap: goToSelectImage,
-        //),
+        bottomNavigationBar: Container(
+          alignment: Alignment.center,
+          height: 64,
+          width: 80,
+          color: Colors.grey[200],
+          child: InkWell(
+            onTap: () {
+              setState(() {
+                selectImage(context);
+              });
+            },
+            child: Padding(
+              padding: const EdgeInsetsDirectional.fromSTEB(0, 10, 0, 0),
+              child: Column(
+                children: const <Widget>[
+                  Icon(
+                    Icons.camera_alt,
+                    color: Colors.blueGrey,
+                  ),
+                  Text('Add Photo',
+                    style: TextStyle(
+                        fontFamily: "helvetica",
+                        fontSize: 15
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
- //void goToSelectImage(int index) {}
+  File? displayImage(){
+    if(file == null){
+      //makeAnotherFile();
+      return otherFile;
+    }
+    else{
+      return file;
+    }
+  }
 
-  selectImage(parentContext) {
+  createPostInFirestore(String _textTitle, String _textDetailed){
+    //postRef.doc(currentUser.uid).collection("userPosts").doc(postId)
+    //    .set({
+    //  "postId" : postId,
+    //  "ownerId" : currentUser.uid,
+    //  "title" : _textTitle,
+    //  "detailedText": _textDetailed,
+    //});
+  }
+
+  void handleSubmit() async{
+    setState(() {
+      isUploading = true;
+    });
+    // uploading photo can be handled here
+    createPostInFirestore(
+      textTitle.text,
+      textDetailed.text,
+    );
+
+  }
+  void makeAnotherFile() async {
+    otherFile = await getImageFileFromAssets('assets/blank_image.png');
+  }
+  Future<File> getImageFileFromAssets(String path) async {
+    final byteData = await rootBundle.load('assets/$path');
+
+    final _file = File('${(await getTemporaryDirectory()).path}/$path');
+    await _file.writeAsBytes(byteData.buffer.asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
+
+    return _file;
+  }
+  selectImage(parentContext) { //todo: BUG it opens first dialog option instead of opening the dialogBox
     return showDialog(
       context: parentContext,
-      builder: (dialogContext) {
+      builder: (parentContext) {
         return SimpleDialog(
           title: const Text("Choose Photo",
             style: TextStyle(
@@ -138,15 +212,6 @@ class _PostState extends State<Post> {
           ),
           children: <Widget> [
             SimpleDialogOption(
-              child: const Text("Capture with Camera",
-                style: TextStyle(
-                    fontSize: 18,
-                    fontFamily: "helvetica"
-                ),
-              ),
-              onPressed: takePhoto(),
-            ),
-            SimpleDialogOption(
               child: const Text("Select from Gallery",
                 style: TextStyle(
                   fontSize: 18,
@@ -154,6 +219,15 @@ class _PostState extends State<Post> {
                 ),
               ),
               onPressed: chooseFromGallery(),
+            ),
+            SimpleDialogOption(
+              child: const Text("Capture with Camera",
+                style: TextStyle(
+                    fontSize: 18,
+                    fontFamily: "helvetica"
+                ),
+              ),
+              onPressed: takePhoto(),
             ),
             SimpleDialogOption(
               child: const Text("Cancel",
@@ -176,6 +250,7 @@ class _PostState extends State<Post> {
     dynamic _file = await ImagePicker().pickImage(source: ImageSource.gallery);
     setState(() {
       file = _file;
+      fileChosen = true;
     });
   }
   takePhoto() async {
@@ -184,11 +259,12 @@ class _PostState extends State<Post> {
         source: ImageSource.camera, maxHeight: 67500, maxWidth: 96000);
     setState(() {
       file = _file;
+      fileChosen = true;
     });
   }
   bool doesImageExist(){
     if(file == null) {
-        return false;
+      return false;
     }
     else {
       return true;
